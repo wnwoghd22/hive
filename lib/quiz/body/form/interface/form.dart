bad1f022f57ce312acee809dcc0d7c9bfe8eb21c
wnwoghd22 <wnwoghd22@gmail.com>
2022-02-26 14:49:23 +0900

    modifying quizform

--- lib/quiz/body/form/interface/form.dart
+++ lib/quiz/body/form/interface/form.dart
@@ -1,3 +1,14 @@
-abstract class QuizForm {
+abstract class IQuiz {
   bool check();
 }
+
+/*
+abstract class QuizForm extends StatefulWidget implements IQuiz {
+
+}
+
+class QuizFormState<T extends QuizForm> extends State<T> {
+
+}
+
+*/
