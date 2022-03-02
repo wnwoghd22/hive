0704d74c2fbce161616db461ef769e202ab55152
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2022-03-03 02:07:50 +0900

    add link

--- lib/components/sidebar.dart
+++ lib/components/sidebar.dart
@@ -1,10 +1,25 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
 
 class Sidebar extends StatelessWidget {
   const Sidebar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: ListView(
+        children: [
+          Link(
+            uri: Uri.parse('https://drone.onestop.go.kr/'),
+            builder: (context, onPressed) => ElevatedButton(
+              onPressed: onPressed,
+              child: const Text('원스톱 서비스')
+            )
+          )
+        ],
+      )
+    );
   }
 }
