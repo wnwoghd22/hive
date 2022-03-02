3370659550109788ea2df95852be6878c7121fe5
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2022-03-02 22:28:24 +0900

    Update page.dart
    BottomNavigationBar has bug...
    Selection Effect does not work.

--- lib/top/page.dart
+++ lib/top/page.dart
@@ -17,6 +17,12 @@ class _TopPageState extends State<TopPage> {
   late Widget _page3;
   late int _currentIndex;
   late Widget _currentPage;
+  late int _course;
+  late Widget _bottomBar1;
+  late Widget _bottomBar2;
+  late List<Widget> _bottomBars;
+  late Widget _bottomBar;
+  late List<DropdownMenuItem<int>> _courseItems;
 
   @override
   void initState() {
@@ -27,6 +33,61 @@ class _TopPageState extends State<TopPage> {
     _pages = [ _page1, _page2, _page3 ];
     _currentIndex = 0;
     _currentPage = _page1;
+    _course = 0;
+    _bottomBar1 = BottomNavigationBar(
+        onTap: (index) {
+          _changeTab(index);
+        },
+        currentIndex: _currentIndex,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.book),
+              label: '퀴즈풀이'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.assignment),
+              label: '기출문제'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.announcement),
+              label: '구술평가'
+          ),
+        ]
+    );
+    _bottomBar2 = BottomNavigationBar(
+        onTap: (index) {
+          _changeTab(index);
+        },
+        currentIndex: _currentIndex,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.book),
+              label: 'Part 1'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.book),
+              label: 'Part 2'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.assignment),
+              label: '기출문제'
+          ),
+        ]
+    );
+    _bottomBars = [ _bottomBar1, _bottomBar2 ];
+
+    List<String> courseList = ['조종자', '지도조종자' ];
+    _courseItems = [];
+    for (int i = 0; i < courseList.length; ++i) {
+      _courseItems.add(
+        DropdownMenuItem(
+          child: Text(courseList[i]),
+          value: i,
+        )
+      );
+    }
+
+    _bottomBar = _bottomBars[0];
   }
 
   void _changeTab(int index) {
@@ -36,34 +97,32 @@ class _TopPageState extends State<TopPage> {
     });
   }
 
+  void _selectCourse(int value) {
+    setState(() {
+      _bottomBar = _bottomBars[value];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('드론 문제풀이')
+          title: Text('드론 문제풀이'),
+          actions: [
+            DropdownButton<int>(
+              value: _course,
+              items: _courseItems,
+              onChanged: (value) {
+                setState(() {
+                  _course = value!;
+                });
+                _selectCourse(_course);
+              })
+          ]
       ),
         body: _currentPage,
-        bottomNavigationBar: BottomNavigationBar(
-          onTap: (index) {
-            _changeTab(index);
-          },
-          currentIndex: _currentIndex,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.book),
-              label: '퀴즈풀이'
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.assignment),
-              label: '기출문제'
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.announcement),
-              label: '구술평가'
-            ),
-          ]
-        )
+        bottomNavigationBar: _bottomBar
       )
     );
   }
